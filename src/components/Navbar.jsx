c04eb948bb08c622738f5b7455857a5d84cd9603
Feh <felipemuramatsu02@gmail.com>
2024-08-05 16:30:27 -0300

    updated project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import icon from "../assets/logo-arch.svg";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex flex-row justify-between md:px-32 p-4 m-2">
+      <div>
+        <img src={icon} alt="logo" />
+      </div>
+      <nav className="flex gap-4">
+        <a href="/works">How it Works</a>
+        <a href="/gallery">Design Gallery</a>
+        <a href="/Architects">Architects</a>
+        <a href="/Articles">Articles</a>
+      </nav>
+      <div>
+        <a href="/signIn">Sign In</a>
+        <button className="">Sign Up</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
