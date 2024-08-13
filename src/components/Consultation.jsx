a60ceb38937b28df2d5b751933ce21ed880d3cbe
Feh <felipemuramatsu02@gmail.com>
2024-08-13 16:23:05 -0300

    updated project

--- src/components/Consultation.jsx
+++ src/components/Consultation.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import img from "../assets/consultationImg.svg";
+import Button from "../layout/Button";
 
 const Consultation = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div>
+        <div>
+          <h1>Realize your dream project with Archmove</h1>
+          <Button title="FREE CONSULTATION" />
+        </div>
+      </div>
     </div>
   );
 };
